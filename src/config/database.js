c80639805e5a776b48ff7c047757d51a475728f8
Danny Medina Coronel <dannymedinacoronel@gmail.com>
2021-09-30 23:24:42 +0200

    3on commit

---
+++ src/config/database.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+
+module.exports = {
+	'url': 'mongodb://localhost/login-node'
+
+
+};
+
+
+
+
+
+// module.exports = {
+//	'url': 'mongodb-srv://dannymedinacoronel:<A1g9g50/*>@cluster0.4dcl9.mongodb.net:27017/login-node'
+
+
+//};
+
+//mongoose.connect('mongodb://dannymedinacoronel:A1g9g50/*@cluster0.4dcl9.mongodb.net/login-node')
+
+
+
+//module.exports = {
+//	'url': 'mongodb//dannymedinacoronel:A1g9g50%2F%2a@cluster0.4dcl9.mongodb.net/login-node/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
+
+//};
