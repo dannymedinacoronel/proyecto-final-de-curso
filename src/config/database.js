2a7aa17ce3c1422eb9b2b4c8cea2c051c7f6dd77
Danny Medina Coronel <dannymedinacoronel@gmail.com>
2021-10-01 20:30:17 +0200

    commit final 2

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,19 @@
-const mongoose = require("mongoose");
+//const mongoose = require("mongoose");
 
 
-module.exports = {
-	'url': 'mongodb://localhost/login-node'
+//module.exports = {
+//	'url': 'mongodb://localhost/login-node'
 
+ 
+//};
 
-};
-
-
+var MongoClient = require('mongodb').MongoClient;
+var uri = "mongodb://dannymedinacoronel:<A1g9g50/*>@cluster0-shard-00-00.4dcl9.mongodb.net:27017,cluster0-shard-00-01.4dcl9.mongodb.net:27017,cluster0-shard-00-02.4dcl9.mongodb.net:27017/?ssl=true&replicaSet=atlas-jr6zu6-shard-0&authSource=admin&retryWrites=true&w=majority";
+MongoClient.connect(uri, function(err, client) {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 
 
